21f403c186f9181810c48999abb82b7b82d43867
Sem8 <mssemmi8@gmail.com>
2019-07-18 16:05:17 -0400

    implemented stock prices function that works and passes all tests but can not return -10 but only returns 0 instead so does not pass the required tests

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,21 +2,39 @@
 
 import argparse
 
+# My solution that I understand
 def find_max_profit(prices):
-  buy_price = 0
+  buy_price = prices[0]
   sell_price = 0
+  profit = -1000000
 
-  for i in range(len(prices)-1):
-    i_index = i
-    for j in range(i + 1, len(prices)-1):
-      if prices[i+1] > prices[i_index]:
-        buy_price = prices[i_index]
-        sell_price = prices[j]
-        difference = sell_price - buy_price
-        if prices[i+1] < buy_price:
-          prices[i+1] = buy_price
-        j += 1        
-        return difference
+  for i in range(len(prices)):
+    each_price = prices[i]
+
+    if each_price < buy_price:
+      buy_price = each_price
+      sell_price = 0
+    elif each_price > sell_price:
+      sell_price = each_price
+      profit = sell_price - buy_price
+  return profit
+
+# Clunky solution to pass the test
+def find_max_profit(prices):
+
+
+
+price1 = [10, 7, 5, 8, 11, 9]
+price2 = [100, 90, 80, 50, 20, 10]
+price3 = [1050, 270, 1540, 3800, 2]
+price4 = [100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79] 
+
+print(find_max_profit(price1))
+print(find_max_profit(price2))
+print(find_max_profit(price3))
+print(find_max_profit(price4))
+
+
 
 
 if __name__ == '__main__':
